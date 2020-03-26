bffe8ab9cc215123f2ce210ee89a8239ecd32048
Aliaksand Shynkevich <aliaksandr_shynkevich@epam.com>
2020-03-26 23:37:45 +0300

    Adjusted service logic

--- src/main/java/by/shynkevich/math/example/controller/WelcomeController.java
+++ src/main/java/by/shynkevich/math/example/controller/WelcomeController.java
@@ -1,8 +1,10 @@
-package by.shynkevich.math;
+package by.shynkevich.math.example.controller;
 
+import java.util.List;
 import java.util.Objects;
 import javax.servlet.http.HttpSession;
 
+import by.shynkevich.math.example.domain.TypicalExample;
 import by.shynkevich.math.example.service.ExampleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,31 +21,36 @@ public class WelcomeController {
     private static final String SERVICE_KEY = "service";
     private static final String WELCOME_VIEW = "welcome";
     private static final String EXAMPLE_TABLE_VIEW = "exampleTable";
+    private static final String EXAMPLES_KEY = "examples";
 
     @GetMapping("/")
     public String main() {
         return WELCOME_VIEW;
     }
 
-    @GetMapping("/examples")
-    public String main(
-            @RequestParam(name = "count", defaultValue = "10") int count,
-            @RequestParam(name = "minLimit", defaultValue = "0") int minLimit,
-            @RequestParam(name = "maxLimit", defaultValue = "10") int maxLimit,
-            Model model, HttpSession session) {
-
+    @PostMapping("/examples")
+    public String main(@RequestParam(name = "count", defaultValue = "10") int count,
+                       @RequestParam(name = "minLimit", defaultValue = "0") int minLimit,
+                       @RequestParam(name = "maxLimit", defaultValue = "10") int maxLimit,
+                       Model model, HttpSession session) {
         ExampleService service = extractService(session);
-        service.init(count, minLimit, maxLimit);
-        model.addAttribute("examples", service.getExamples());
+        List<TypicalExample> examples = service.init(count, minLimit, maxLimit);
+        model.addAttribute(EXAMPLES_KEY, examples);
         return EXAMPLE_TABLE_VIEW;
     }
 
+    @GetMapping("/examples")
+    public ResponseEntity<Boolean> checkTotal(HttpSession session) {
+        ExampleService service = extractService(session);
+        return new ResponseEntity<>(service.allResolved(), HttpStatus.OK);
+    }
+
     @PostMapping("/examples/{id}")
-    public ResponseEntity<Boolean> checkResult(
-            @PathVariable("id") String id,
-            @RequestParam("value") int value,
-            HttpSession session) {
-        return new ResponseEntity<>(extractService(session).checkResult(id, value), HttpStatus.OK);
+    public ResponseEntity<Boolean> checkResult(@PathVariable("id") String id,
+                                               @RequestParam("value") int value,
+                                               HttpSession session) {
+        boolean isSuccess = extractService(session).checkResult(id, value);
+        return new ResponseEntity<>(isSuccess, HttpStatus.OK);
     }
 
     private ExampleService extractService(HttpSession session) {
--- src/main/java/by/shynkevich/math/example/service/ExampleService.java
+++ src/main/java/by/shynkevich/math/example/service/ExampleService.java
@@ -1,8 +1,10 @@
 package by.shynkevich.math.example.service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -14,21 +16,29 @@ import org.springframework.stereotype.Service;
 public class ExampleService {
 
     private Map<String, TypicalExample> exampleMap;
+    private Set<String> success;
 
-    public void init(int count, int minLimit, int maxLimit) {
+    public List<TypicalExample> init(int count, int minLimit, int maxLimit) {
         exampleMap = IntStream.range(0, count)
                 .mapToObj(i -> ExampleFactory.createExample(minLimit, maxLimit))
                 .collect(Collectors.toMap(TypicalExample::getId, entry -> entry));
-    }
-
-    public List<TypicalExample> getExamples() {
+        success = new HashSet<>();
         return new ArrayList<>(exampleMap.values());
     }
 
     public boolean checkResult(String id, int value) {
-        return exampleMap.get(id)
+        boolean isSuccess = exampleMap.get(id)
                 .getTerms()
                 .stream()
                 .anyMatch(term -> term.isHidden() && value == term.getValue());
+
+        if (isSuccess) {
+            success.add(id);
+        }
+        return isSuccess;
+    }
+
+    public boolean allResolved() {
+        return exampleMap.size() == success.size();
     }
 }
