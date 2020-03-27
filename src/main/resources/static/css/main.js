68d0fd94734a972d05edfbca7315a833a42a8b23
Aliaksandr Shynkevich <aliaksandr_shynkevich@epam.com>
2020-03-27 16:46:59 +0300

    Extracted JS script

---
+++ src/main/resources/static/css/main.js
@@ -0,0 +1,27 @@
+function check(elem) {
+    var but = $(elem);
+    var id = but.attr("id");
+    var inputField = $(`input[id="${id}"]`);
+    var val = inputField.val();
+    if (val) {
+        $.post("/examples/" + id, {
+            value: val
+        }, function(data) {
+            if (data) {
+                but.text("Правильно!");
+                but.attr('class', 'btn btn-success');
+                but.attr('disabled', true);
+                inputField.attr('disabled', true);
+                $.get("/examples", function(data) {
+                    if (data) {
+                        $('#scoreModal').modal();
+                    }
+                });
+            } else {
+                $('#wrongModal').modal();
+                but.text("Неверно!");
+                but.attr('class', 'btn btn-danger');
+            }
+        });
+    }
+}
