048f8b82234dd93909b9a3265b2c843ef1a226c3
Aliaksandr Shynkevich <aliaksandr_shynkevich@epam.com>
2020-05-02 12:03:49 +0300

    Added browser flag

--- src/main/java/by/shynkevich/math/MathApplication.java
+++ src/main/java/by/shynkevich/math/MathApplication.java
@@ -10,16 +10,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MathApplication {
+
+    private static final String BROWSER_PROPERTY = "browser";
+    private static final String LOCALHOST = "http://localhost:8080/";
+
     public static void main(String[] args) {
         SpringApplication.run(MathApplication.class, args);
         openHomePage();
     }
 
     private static void openHomePage() {
-        System.setProperty("java.awt.headless", "false");
-        if (Desktop.isDesktopSupported()) {
+        if (Boolean.parseBoolean(System.getProperty(BROWSER_PROPERTY))) {
             try {
-                URI homepage = new URI("http://localhost:8080/");
+                System.setProperty("java.awt.headless", "false");
+                URI homepage = new URI(LOCALHOST);
                 Desktop desktop = Desktop.getDesktop();
                 desktop.browse(homepage);
             } catch (URISyntaxException | IOException e) {
